259369a2c60854ffcfd19d2789bdf98814645fc4
Abbie <jdriveraramirez@gmail.com>
2020-05-29 14:41:37 -0600

    Variables added, website is responsive

--- js/home.js
+++ js/home.js
@@ -1,2 +1,19 @@
-alert('Connected')
+const name = document.querySelector('#name'),
+      number = document.querySelector('#number'),
+      direction = document.querySelector('#direction'),
+      zone = document.querySelector('#zone')
+
+const camaron = document.querySelector('#camaron'),
+      colita = document.querySelector('#colita'),
+      filete = document.querySelector('#filete'),
+      cabeza = document.querySelector('#cabeza')
+
+const tilapiaUnits = document.querySelector('#tilapiaUnits'),
+      tilapiaCant = document.querySelector('#tilapiaCant'),
+      bocaUnits = document.querySelector('#bocaUnits'),
+      bocaCant = document.querySelector('#bocaCant')
+
+// swal("Here's the title!", "", "info");
+
+// alert('Connected')
 // Make an alert with the order details
